ab0c82ef65ecccab864cc0eec427d52de87e3f0c
Obiekwe Agnes-Mary Chinaza <50022245+chinazaaa@users.noreply.github.com>
2024-06-30 23:30:13 +0100

    Phelicks did this

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:sharepact_app/app_theme.dart';
 import 'splash.dart';
 import 'onboarding.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
+  @override 
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SharePact',
